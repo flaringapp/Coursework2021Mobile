7877f620dd41757cbbe02155253ef63cd5aea2a2
Mallivance <ashpek01@gmail.com>
2021-04-27 15:53:49 +0300

    Add mocked profile source model

---
+++ app/src/main/java/com/flaringapp/coursework2021/mock/ProfileSourceModelMock.kt
@@ -0,0 +1,20 @@
+package com.flaringapp.coursework2021.mock
+
+import com.flaringapp.coursework2021.data.common.call.CallResult
+import com.flaringapp.coursework2021.data.network.features.profile.ProfileSourceModel
+import com.flaringapp.coursework2021.data.network.features.profile.response.LoginResponse
+
+class ProfileSourceModelMock : ProfileSourceModel {
+
+    override suspend fun login(login: String, password: String): CallResult<LoginResponse> {
+        return CallResult.Success(
+            LoginResponse(
+                "1",
+                "Name",
+                "Surname",
+                "namesurname@gmail.com",
+                "token_namesurname"
+            )
+        )
+    }
+}
