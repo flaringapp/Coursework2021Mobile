d9139bdd9cdf13002763ad1b64ad940adf5113fb
Mallivance <ashpek01@gmail.com>
2021-04-27 16:39:23 +0300

    Remove floor entity

--- app/src/main/java/com/flaringapp/coursework2021/data/repository/entity/models/Floor.kt
+++
@@ -1,9 +0,0 @@
-package com.flaringapp.coursework2021.data.repository.entity.models
-
-class Floor(
-    val id: String,
-    val locationId: String,
-    val name: String,
-    val description: String,
-    val orderNumber: Int,
-)
