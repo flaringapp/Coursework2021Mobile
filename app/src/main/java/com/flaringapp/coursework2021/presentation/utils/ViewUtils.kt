e182ec2ddc679e9226b4e5c57becff2cc44303dd
Mallivance <ashpek01@gmail.com>
2021-04-28 16:03:24 +0300

    Add amount picker

--- app/src/main/java/com/flaringapp/coursework2021/presentation/utils/ViewUtils.kt
+++ app/src/main/java/com/flaringapp/coursework2021/presentation/utils/ViewUtils.kt
@@ -1,6 +1,9 @@
 package com.flaringapp.coursework2021.presentation.utils
 
+import android.content.Context
+import android.content.res.Resources
 import android.graphics.drawable.Drawable
+import android.util.TypedValue
 import android.widget.TextView
 import androidx.core.view.isVisible
 
@@ -23,4 +26,54 @@ fun TextView.updateCompoundDrawablesWithIntrinsicBounds(
         right ?: this.compoundDrawables[2],
         bottom ?: this.compoundDrawables[3],
     )
+}
+
+
+
+fun Context.dpi(dp: Float): Int {
+    return resources.dp(dp).toInt()
+}
+
+fun Context.dpi(dp: Int): Int {
+    return resources.dp(dp).toInt()
+}
+
+fun Context.dp(dp: Float): Float {
+    return resources.dp(dp)
+}
+
+fun Context.dp(dp: Int): Float {
+    return resources.dp(dp)
+}
+
+fun Context.spi(sp: Float): Int {
+    return resources.sp(sp).toInt()
+}
+
+fun Context.spi(sp: Int): Int {
+    return resources.sp(sp).toInt()
+}
+
+fun Context.sp(sp: Float): Float {
+    return resources.sp(sp)
+}
+
+fun Context.sp(sp: Int): Float {
+    return resources.sp(sp)
+}
+
+fun Resources.dp(dp: Float): Float {
+    return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, displayMetrics)
+}
+
+fun Resources.dp(dp: Int): Float {
+    return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp.toFloat(), displayMetrics)
+}
+
+fun Resources.sp(sp: Float): Float {
+    return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, sp, displayMetrics)
+}
+
+fun Resources.sp(sp: Int): Float {
+    return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, sp.toFloat(), displayMetrics)
 }
