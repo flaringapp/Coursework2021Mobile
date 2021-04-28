fa894e9f47197d8e103095e57a66b317a5ec61b1
Mallivance <ashpek01@gmail.com>
2021-04-28 17:17:00 +0300

    Add toolbar

--- app/src/main/java/com/flaringapp/coursework2021/presentation/features/main/MainActivity.kt
+++ app/src/main/java/com/flaringapp/coursework2021/presentation/features/main/MainActivity.kt
@@ -2,11 +2,27 @@ package com.flaringapp.coursework2021.presentation.features.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.flaringapp.coursework2021.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        setupToolbarUI()
+    }
+
+    private fun setupToolbarUI() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainer) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        findViewById<Toolbar>(R.id.toolbar)
+            .setupWithNavController(navController, appBarConfiguration)
     }
 }
