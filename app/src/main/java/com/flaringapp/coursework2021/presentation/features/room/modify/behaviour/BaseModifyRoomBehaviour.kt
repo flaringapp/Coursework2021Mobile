a19b7618d949a5494eeb5f8d5e77db5841ed51e0
Mallivance <ashpek01@gmail.com>
2021-04-28 18:39:33 +0300

    Cleanup room behaviour

--- app/src/main/java/com/flaringapp/coursework2021/presentation/features/room/modify/behaviour/BaseModifyRoomBehaviour.kt
+++ app/src/main/java/com/flaringapp/coursework2021/presentation/features/room/modify/behaviour/BaseModifyRoomBehaviour.kt
@@ -1,11 +1,15 @@
 package com.flaringapp.coursework2021.presentation.features.room.modify.behaviour
 
 import com.flaringapp.coursework2021.data.repository.entity.EntityRepository
+import com.flaringapp.coursework2021.data.repository.entity.models.Room
 import com.flaringapp.coursework2021.presentation.features.room.modify.models.RoomEditableData
 import kotlinx.parcelize.IgnoredOnParcel
 import org.koin.core.context.GlobalContext
 
 abstract class BaseModifyRoomBehaviour: ModifyRoomBehaviour {
+
+    @IgnoredOnParcel
+    override val room: Room? = null
 
     @IgnoredOnParcel
     protected val repository: EntityRepository by GlobalContext.get().inject()
--- app/src/main/java/com/flaringapp/coursework2021/presentation/features/room/modify/behaviour/CreateRoomBehaviour.kt
+++ app/src/main/java/com/flaringapp/coursework2021/presentation/features/room/modify/behaviour/CreateRoomBehaviour.kt
@@ -2,14 +2,10 @@ package com.flaringapp.coursework2021.presentation.features.room.modify.behaviou
 
 import com.flaringapp.coursework2021.data.common.call.CallResultNothing
 import com.flaringapp.coursework2021.data.repository.entity.models.Room
-import kotlinx.parcelize.IgnoredOnParcel
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 class CreateRoomBehaviour: BaseModifyRoomBehaviour() {
-
-    @IgnoredOnParcel
-    override val room: Room? = null
 
     override suspend fun modifyRoom(room: Room): CallResultNothing {
         return repository.addRoom(room)
