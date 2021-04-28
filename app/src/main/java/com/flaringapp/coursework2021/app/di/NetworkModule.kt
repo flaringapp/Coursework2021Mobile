70e900c0676f5eca541f63e6f1fd13bfeaf8f7ca
Mallivance <ashpek01@gmail.com>
2021-04-28 11:59:49 +0300

    Setup mock dependencies

--- app/src/main/java/com/flaringapp/coursework2021/app/di/NetworkModule.kt
+++ app/src/main/java/com/flaringapp/coursework2021/app/di/NetworkModule.kt
@@ -5,6 +5,8 @@ import com.flaringapp.coursework2021.data.network.features.buildings.BuildingsSo
 import com.flaringapp.coursework2021.data.network.features.buildings.BuildingsSourceModelImpl
 import com.flaringapp.coursework2021.data.network.features.profile.ProfileSourceModel
 import com.flaringapp.coursework2021.data.network.features.profile.ProfileSourceModelImpl
+import com.flaringapp.coursework2021.mock.BuildingsSourceModelMock
+import com.flaringapp.coursework2021.mock.ProfileSourceModelMock
 import org.koin.dsl.module
 
 val NetworkModule = module {
@@ -14,7 +16,10 @@ val NetworkModule = module {
     single { adapter.profileService }
     single { adapter.buildingsService }
 
-    single<ProfileSourceModel> { ProfileSourceModelImpl(get()) }
-    single<BuildingsSourceModel> { BuildingsSourceModelImpl(get()) }
+//    single<ProfileSourceModel> { ProfileSourceModelImpl(get()) }
+//    single<BuildingsSourceModel> { BuildingsSourceModelImpl(get()) }
+
+    single<ProfileSourceModel> { ProfileSourceModelMock() }
+    single<BuildingsSourceModel> { BuildingsSourceModelMock() }
 
 }
